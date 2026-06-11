e9b4439de590fb427c09fce2d039048e61254a12
legolordr <Ishmametev_den@mail.ru>
2026-06-11 15:51:25 +0500

    add profileCard component with GetRequest and add in home

--- Dota2Stat/Models/Profile.cs
+++ Dota2Stat/Models/Profile.cs
@@ -1,6 +1,35 @@
-﻿namespace Dota2Stat.Models;
+﻿using System.Text.Json.Serialization;
 
-public class Profile
+namespace Dota2Stat.Models;
+/// <summary>
+/// https://docs.opendota.com/#tag/players
+/// </summary>
+public class RootObject
 {
-    
+    [JsonPropertyName("profile")]
+    public PlayerProfile? Profile { get; set; }
+
+    [JsonPropertyName("computed_mmr")]
+    public int? ComputedMmr { get; set; }
+
+    [JsonPropertyName("rank_tier")]
+    public int? RankTier { get; set; }
+
+    [JsonPropertyName("leaderboard_rank")]
+    public int? LeaderboardRank { get; set; }
+}
+
+public class PlayerProfile
+{
+    [JsonPropertyName("account_id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; } = string.Empty;
+
+    [JsonPropertyName("avatarfull")]
+    public string AvatarFull { get; set; } = string.Empty;
+
+    [JsonPropertyName("profileurl")]
+    public string ProfileUrl { get; set; } = string.Empty;
 }
--- Dota2Stat/Program.cs
+++ Dota2Stat/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
@@ -15,6 +16,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
