b7d9652ee1a6b2b1f147969affe4553e6e5bc023
legolordr <Ishmametev_den@mail.ru>
2026-06-13 16:38:57 +0500

    Add RecentMatches

---
+++ Dota2Stat/Models/Hero.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Dota2Stat.Models;
+
+public class Hero
+{
+    [JsonPropertyName("id")]
+    public long Id { get; set; }
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+    [JsonPropertyName("localized_name")]
+    public string LocalizedName { get; set; }
+}
---
+++ Dota2Stat/Models/RecentMatches.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Dota2Stat.Models;
+
+public class RecentMatch
+{
+    [JsonPropertyName("match_id")]
+    public long MatchId { get; set; }
+
+    [JsonPropertyName("hero_id")]
+    public long HeroId { get; set; }
+
+    [JsonPropertyName("kills")]
+    public long Kills { get; set; }
+
+    [JsonPropertyName("deaths")]
+    public long Deaths { get; set; }
+
+    [JsonPropertyName("assists")]
+    public long Assists { get; set; }
+
+    [JsonPropertyName("duration")]
+    public long Duration { get; set; }
+
+    [JsonPropertyName("radiant_win")]
+    public bool RadiantWin { get; set; }
+
+    [JsonPropertyName("player_slot")]
+    public long PlayerSlot { get; set; }
+
+    [JsonPropertyName("game_mode")]
+    public long GameMode { get; set; }
+
+    [JsonPropertyName("lobby_type")]
+    public long LobbyType { get; set; }
+
+    [JsonPropertyName("start_time")]
+    public long StartTime { get; set; }
+
+    [JsonPropertyName("gold_per_min")]
+    public long GoldPerMin { get; set; }
+
+    [JsonPropertyName("xp_per_min")]
+    public long XpPerMin { get; set; }
+
+    [JsonPropertyName("last_hits")]
+    public long LastHits { get; set; }
+
+    [JsonPropertyName("denies")]
+    public long Denies { get; set; }
+}
